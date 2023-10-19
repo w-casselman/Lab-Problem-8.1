4e24bfa69ad651f0f18bc3d143d026b8ef3057fe
Wesley Casselman <w-casselman@onu.edu>
2023-10-19 09:53:53 -0400

    Completed countCharacter function

--- P8-1/P8-1.cpp
+++ P8-1/P8-1.cpp
@@ -1,1 +1,26 @@
 // Lab Problem 8.1
+// Wesley Casselman
+// October 19th, 2023
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countCharacter(string str);
+
+int main(void)
+{
+	cout << "Enter string: ";
+	string str;
+	getline(cin, str);
+
+	cout << endl << countCharacter(str) << endl;
+
+	return 0;
+}
+
+int countCharacter(string str)
+{
+	return str.length();
+}
